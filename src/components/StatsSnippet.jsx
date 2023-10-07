0927f45fe9b874f839bdf2f41d7f2bac5573b2d9
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-10-07 03:40:38 +0100

    text with picture section left and right

--- src/components/StatsSnippet.jsx
+++ src/components/StatsSnippet.jsx
@@ -6,10 +6,10 @@ const StatsSnippet = ({ heading, value }) => {
         <div className="w-max grid gap-4 px-5 border-r border-[#ececec] [&:nth-of-type(1)]:pl-0 [&:nth-last-of-type(1)]:pr-0 
             [&:nth-last-of-type(1)]:border-none"
         >
-            <h2 className="heading-2 text-dark-19">
+            <h2 className="body-text-2 md:heading-2 text-dark-19">
                 {heading}
             </h2>
-            <p className="body-text-4 text-grey-89">
+            <p className="body-text-6 md:body-text-4 text-grey-89">
                 {value}
             </p>
         </div>
---
+++ src/data/homeData.js
@@ -0,0 +1,16 @@
+import businessman from "../assets/images/businessman.png";
+import worktable from "../assets/images/worktable.png";
+
+export const feature1 = {
+    image: businessman,
+    heading: "Reliable Internet Speeds, No Matter Where You Are!",
+    text: "Stay connected without compromise. Our network ensures consistent, fast internet speeds wherever you are!",
+    btnText: "Get Started"
+}
+
+export const feature2 = {
+    image: worktable,
+    heading: "Powerful Features, Seamless Connectivity",
+    text: "Experience innovation with our powerful features and seamless connectivity that set us apart.",
+    btnText: "Get Started"
+}
