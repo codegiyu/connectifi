f8aa65e09c3ac7814c6385f8bc0b1182317d1208
Edward-Precious Omegbu <eomegbu@gmail.com>
2023-10-15 00:07:32 +0100

    activated mousewheel on top deal slider and two faq data

--- src/components/FaqSingle.jsx
+++ src/components/FaqSingle.jsx
@@ -10,9 +10,9 @@ const FaqSingle = ({ question, answer }) => {
     }
 
     return (
-        <div className="w-full grid overflow-hidden">
-            <div className={`w-full flex justify-between items-center gap-[10px] px-2 py-4 lg:p-4 border-[#AEAEAE] \
-            ${answerOpen ? "border-[0.5px]" : "border-none"} rounded-lg`}>
+        <div className="w-full grid overflow-hidden bg-[#F4F4F4] rounded-[10px]">
+            <div className={`w-full bg-white flex justify-between items-center gap-[10px] px-2 py-4 lg:p-4 border-[#F4F4F4] \
+                ${answerOpen ? "border" : ""} rounded-lg`}>
                 <h4 className="body-text-6 md:body-text-3 font-medium text-dark-19">
                     {question}
                 </h4>
@@ -20,8 +20,8 @@ const FaqSingle = ({ question, answer }) => {
                     <img src={arrow} alt="" className={`w-6 transition-all duration-500 ease-in-out ${answerOpen ? "rotate-180" : "rotate-0"}`} />
                 </button>
             </div>
-            <div className={`w-full pl-8 pr-8 md:pr-12 lg:pr-16 transition-all duration-500 ease-in-out overflow-hidden ${answerOpen ?
-                "max-h-fit opacity-100 py-5" : "max-h-0 opacity-20"}`}>
+            <div className={`w-full px-2 lg:px-4 transition-all duration-500 ease-in-out overflow-hidden ${answerOpen ?
+                "max-h-fit opacity-100 pt-5 pb-6" : "max-h-0 opacity-20"}`}>
                 <p className="body-text-6-medium lg:body-text-4 text-grey-89">
                     {answer}
                 </p>
